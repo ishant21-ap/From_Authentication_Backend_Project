6b523cd656901009dd3aa2e628057f6c9b1356fa
ishant_rathi2104 <121746195+ishant21-ap@users.noreply.github.com>
2024-12-10 09:53:59 +0530

    SecurityConfig.java

--- UserRegisteration/src/main/java/com/project/UserRegisteration/config/SecurityConfig.java
+++ UserRegisteration/src/main/java/com/project/UserRegisteration/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         http.csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/users/register", "/api/users/login", "api/users/forgot-password",
-                                "/api/users/reset-password")
+                                "/api/users/reset-password", "/api/users/verify")
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
